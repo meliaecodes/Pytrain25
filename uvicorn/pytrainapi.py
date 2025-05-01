2e735ff139c2a1836fa09028d37c436e40009c8e
Melissa Paisley <mpaisley@atlassian.com>
2025-05-01 20:53:18 +1000

    iterating

--- uvicorn/pytrainapi.py
+++ uvicorn/pytrainapi.py
@@ -18,8 +18,20 @@ def hub_filter(device, ad):
 def handle_disconnect(_):
     print("Hub was disconnected.")
 
+    # If the hub disconnects before this program is done,
+    # cancel this program so it doesn't get stuck waiting
+    # forever.
+
+ready_event = asyncio.Event()
+
 def handle_rx(_, data: bytearray):
-    print("Received:", data)
+    if data[0] == 0x01:  # "write stdout" event (0x01)
+        payload = data[1:]
+
+        if payload == b"rdy":
+            ready_event.set()
+        else:
+            print("Received:", payload)
 
 class LegoHubClient:
     def __init__(self):
@@ -32,20 +44,30 @@ class LegoHubClient:
         device = await BleakScanner.find_device_by_filter(hub_filter)
         self.client = BleakClient(device, disconnected_callback=handle_disconnect)
         await self.client.connect()
-        if self.client.is_connected:
-            print("Connected")
+        #if self.client.is_connected:
+        #    print("Connected")
         await self.client.start_notify(UART_TX_CHAR_UUID, handle_rx)
-        self.nus = self.client.services.get_service(UART_SERVICE_UUID)
-        self.rx_char = self.nus.get_characteristic(UART_RX_CHAR_UUID)
-        print(self.rx_char)
+        #self.nus = self.client.services.get_service(UART_SERVICE_UUID)
+        #self.rx_char = self.nus.get_characteristic(UART_RX_CHAR_UUID)
+        #print(self.rx_char)
+
+        #print(f"Characteristic {self.rx_char.uuid} has properties: {self.rx_char.properties}")
+        # Check if writing is allowed
+        #if 'write' in self.rx_char.properties or 'write-without-response' in self.rx_char.properties:
+            #print("Characteristic is writable.")
+        #else:
+            #print("Characteristic is not writable.")
+
+        await self.send(b"fwd")
 
 
     async def send(self, data):
         try:
-            await self.client.write_gatt_char(UART_RX_CHAR_UUID, data, response=True)
+            await self.client.write_gatt_char(UART_RX_CHAR_UUID, b"\x06" + data, response=True)
         except Exception as e:
             # Handle exceptions.
             print(e)
+
 
     async def disconnect(self):
         await self.send(b"d")
@@ -74,23 +96,25 @@ async def root():
 
 @app.get("/accelerate")
 async def root():
-    await trainClient.send(b"a")
+    await trainClient.send(b"fwd")
     await asyncio.sleep(8)
     return {"message": "Accelerate"}
 
 
 @app.get("/reverse")
 async def root():
-    await trainClient.send(b"r")
+    await trainClient.send(b"rev")
     await asyncio.sleep(9)
     return {"message": "Go in reverse"}
 
 @app.get("/stop")
 async def root():
-    await trainClient.send(b"s")
+    await trainClient.send(b"bye")
     return {"message": "Stop"}
 
 @app.get("/lap")
 async def root():
     lights = await trainClient.send(b"l")
     return {"message": "do a lap of the track"}
+
+
